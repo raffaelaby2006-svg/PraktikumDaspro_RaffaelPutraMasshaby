6a1dc3749310d0543defc1f9176ee16034544b78
raffaelaby2006-svg <raffaelaby2006@gmail.com>
2025-10-01 13:41:15 +0700

    Tugas 1

--- jobsheet5/ifCetakKRS24.java
+++ jobsheet5/ifCetakKRS24.java
@@ -8,11 +8,10 @@ public class ifCetakKRS24 {
         System.out.print("Apakah UKT sudah lunas? (true/false): ");
         boolean uktLunas = sc.nextBoolean();
 
-        if (uktLunas) {
-            System.out.println("pembayaran UKT terverifikasi");
-            System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
-        } else {
-            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
-        }
-    }
-}
+         String pesan = (uktLunas) 
+            ? "Pembayaran UKT terverifikasi\nSilakan cetak KRS dan minta tanda tangan DPA"
+            : "Registrasi ditolak. Silakan lunasi UKT terlebih dahulu";
+
+        System.out.println(pesan);
+
+    }}
