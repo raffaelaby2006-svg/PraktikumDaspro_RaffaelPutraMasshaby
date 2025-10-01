1bc589ebb11b0416fdc06a62238f9938eeb7b5e3
raffaelaby2006-svg <raffaelaby2006@gmail.com>
2025-10-01 13:54:45 +0700

    Tugas 2

---
+++ jobsheet5/TugasGanjilGenap24.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+    public class TugasGanjilGenap24 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        
+        System.out.print("Masukkan sebuah angka: ");
+        int angka = sc.nextInt();
+
+        if (angka % 2 == 0) {
+            System.out.println("Genap");
+        } else {
+            System.out.println("Ganjil");
+        }
+    }
+}
+
+
