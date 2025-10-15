8d228c96300ccb3fcea7fe2511b2c4abc165b157
raffaelaby2006-svg <raffaelaby2006@gmail.com>
2025-10-15 11:35:56 +0700

    menambahkan keterangan lulus dan tidak lulus

--- jobsheet5/SiakadFor24.java
+++ jobsheet5/SiakadFor24.java
@@ -3,6 +3,7 @@ public class SiakadFor24 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double nilai, tertinggi = 0, terendah = 100;
+        int lulus = 0, tidakLulus = 0;
         for (int i = 1; i < 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ":" ); 
             nilai = sc.nextDouble();
@@ -13,6 +14,11 @@ public class SiakadFor24 {
                 terendah=nilai;
             
             }
+            if (nilai >= 60)
+             lulus++;
+             else
+             tidakLulus++;
+
         }
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah:" + terendah);
