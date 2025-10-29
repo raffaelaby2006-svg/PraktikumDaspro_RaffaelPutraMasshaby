689cbdd80cf53d3875f69455d38b43caba9080d8
raffaelaby2006-svg <raffaelaby2006@gmail.com>
2025-10-29 13:22:20 +0700

    perubahan 1

---
+++ jobsheet7/NilaiKelompok24.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class NilaiKelompok24 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int i = 1, j;          
+        int nilai;             
+        float totalNilai;      
+        float rataNilai;      
+
+        while (i <= 6) {
+            System.out.println("Kelompok " + i);
+            totalNilai = 0;   
+
+            for (j = 1; j <= 5; j++) {
+                System.out.print("  Nilai dari Kelompok Penilai " + j + ": ");
+                nilai = sc.nextInt();
+                totalNilai += nilai;
+            }
+
+            rataNilai = totalNilai / 5;
+            System.out.println("Kelompok " + i + ": nilai rata-rata = " + rataNilai);
+            System.out.println();
+            i++; 
+        }
+    }
+}
